5f753e63d1f5b1cbc121bb097c3fc8f5abaa09fe
Brian Walsh <brian_walsh_151@yahoo.com>
2017-12-26 14:45:48 -0600

    129 - Logged starting crosshair position

--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -15,18 +15,23 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 	GENERATED_BODY()
 	
 public:
-	ATank* GetControlledTank() const;
-
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 	
 	virtual void BeginPlay() override;
 
 private:
+
+	ATank* GetControlledTank() const;
 	// Start the tank moving the barrel so that a shot would be where
 	// the crosshair intersects the world
 	void AimTowardsCrosshair();
 
 	//Return an OUT parameter, true if hit landscape
 	bool GetSightRayHitLocation(FVector& OutHitLocation) const;
+
+	UPROPERTY(EditAnywhere)
+	float CrossHairXLocation = 0.5f;
+	UPROPERTY(EditAnywhere)
+	float CrossHairYLocation = 0.3333f;
 };
