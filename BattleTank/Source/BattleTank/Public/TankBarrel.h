dda8180121fa4562bbd91dcb59de9fd3017c886c
Brian Walsh <brian_walsh_151@yahoo.com>
2018-01-11 19:56:27 -0600

    141 - Minor renames

--- BattleTank/Source/BattleTank/Public/TankBarrel.h
+++ BattleTank/Source/BattleTank/Public/TankBarrel.h
@@ -8,13 +8,22 @@
 /**
  * 
  */
-UCLASS()
+UCLASS(meta = (BlueprintSpawnableComponent))
 class BATTLETANK_API UTankBarrel : public UStaticMeshComponent
 {
 	GENERATED_BODY()
 	
 public:
-	void Elevate(float DegreesPerSecond);
+	// -1 is max downward speed, and +1 is max up movement
+	void Elevate(float RelativeSpeed);
 	
-	
+private:
+	UPROPERTY(EditAnywhere, Category = Setup)
+	float MaxDegreesPerSecond = 20; // Sensible default
+
+	UPROPERTY(EditAnywhere, Category = Setup)
+	float MaxElevationDegrees = 40; // Sensible default
+
+	UPROPERTY(EditAnywhere, Category = Setup)
+	float MinElevationDegrees = 0; // Sensible default
 };
