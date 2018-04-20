84ccde9e491ddd6493fcfc52280f11096b77b4a9
Brian Walsh <brian_walsh_151@yahoo.com>
2018-04-20 08:09:19 -0500

    186.5 - Took out Begin Play events for Tank Aiming component, as there deprecation warnings.

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -12,8 +12,8 @@ UTankAimingComponent::UTankAimingComponent()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
-	bWantsBeginPlay = true;
-	PrimaryComponentTick.bCanEverTick = true;
+	//bWantsBeginPlay = true;
+	//PrimaryComponentTick.bCanEverTick = true;
 }
 
 void UTankAimingComponent::BeginPlay()
